5b27bae5b1e36af6c79c2085914265a395e8ba12
sahan <gunathungawsd.21@uom.lk>
2024-09-03 21:56:09 +0530

    further Routing completed

--- alumex-project/src/pages/AdminPage/AdminDash.jsx
+++ alumex-project/src/pages/AdminPage/AdminDash.jsx
@@ -1,9 +1,13 @@
 import "./AdminDash.css";
+import { useNavigate } from "react-router-dom";
 
 function AdminDash() {
+  const navigate = useNavigate();
+
   return (
     <div>
-        AdminPage
+      <button onClick={() => navigate('/cast')}>Cast</button>
+      <button onClick={() => navigate('/homoginizedpage')}>Homogenized</button>
     </div>
   );
 }
--- alumex-project/src/pages/HomogenizedPage/HomoginizedPage.jsx
+++ alumex-project/src/pages/HomogenizedPage/HomoginizedPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { alloys } from "../../assets/assset";
 
 export default function HomoginizedPage() {
   return (
     <div>
       Homoginized Page
+      <diV>
+        <ul>
+          {alloys.map((item, index) => (
+            <li key={index}>
+              <span>{item}</span>
+            </li>
+          ))}
+        </ul>
+      </diV>
     </div>
-  )
+  );
 }
