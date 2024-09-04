c0db63f238c8f762f2dbd77fc3abb177c1804cc2
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-09-04 17:50:31 +0530

    form added

--- alumex-project/src/App.jsx
+++ alumex-project/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import CastPage from "./pages/CastPage/CastPage";
+import Login from "./pages/LoginPage/Login";
+import AdminDash from "./pages/AdminPage/AdminDash";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import HomoginizedPage from "./pages/HomogenizedPage/HomoginizedPage";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <div>
-        Alumex
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<AdminDash />} />
+        <Route path="/cast" element={<CastPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/landingpage" element={<LandingPage />} />
+        <Route path="/homoginizedpage" element={<HomoginizedPage />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
