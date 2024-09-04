7fcf738b485f162c94c27af3e572a0dc186f44ab
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-09-04 17:58:04 +0530

    bugs fixed

--- alumex-project/src/App.jsx
+++ alumex-project/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Routes, Route } from "react-router-dom";
 import CastPage from "./pages/CastPage/CastPage";
 import Login from "./pages/LoginPage/Login";
@@ -9,25 +8,6 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<AdminDash />} />
-        <Route path="/cast" element={<CastPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/landingpage" element={<LandingPage />} />
-        <Route path="/homoginizedpage" element={<HomoginizedPage />} />
-        <Route path="/form" element={<Form />} />
-
-import { Routes,Route } from 'react-router-dom'
-import CastPage from './pages/CastPage/CastPage';
-import Login from './pages/LoginPage/Login';
-import AdminDash from './pages/AdminPage/AdminDash';
-import LandingPage from './pages/LandingPage/LandingPage';
-import HomoginizedPage from './pages/HomogenizedPage/HomoginizedPage';
-
-
-function App() {
-  return (
     <div className='app'>
       <Routes>
         <Route path='/' element={<AdminDash/>} />
@@ -35,6 +15,7 @@ function App() {
         <Route path='/login' element={<Login/>} />
         <Route path='/landingpage' element={<LandingPage/>} />
         <Route path='/homoginizedpage' element={<HomoginizedPage/>} />
+        <Route path='/form' element={<Form/>} />
 
       </Routes>
     </div>
