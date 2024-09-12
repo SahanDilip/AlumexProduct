07e9320eb041e4f8e495c3d583095cab26d58d76
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-09-12 22:14:29 +0530

    backup excel sheet added

--- Backend/Controllers/sheet.js
+++ Backend/Controllers/sheet.js
@@ -2,6 +2,13 @@ import { GoogleSpreadsheet } from "google-spreadsheet";
 import { JWT } from "google-auth-library";
 import fs from "fs";
 
+import xlsx from "xlsx";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// ES module equivalent of __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const credentials = JSON.parse(
   fs.readFileSync("carbon-compound-434603-b2-33a8c9212c39.json")
 );
@@ -59,6 +66,41 @@ export const addData = async (req, res, next) => {
 
     const spreadsheet = doc.sheetsByTitle[sheet];
     await spreadsheet.addRow(data);
+
+    const backupDirPath = path.join(__dirname, "assets");
+    if (!fs.existsSync(backupDirPath)) {
+      fs.mkdirSync(backupDirPath, { recursive: true });
+    }
+    const backupFilePath = path.join(backupDirPath, "backup.xlsx");
+
+    let workbook;
+    let worksheet;
+
+    // Check if the backup file exists
+    if (fs.existsSync(backupFilePath)) {
+      workbook = xlsx.readFile(backupFilePath);
+      worksheet = workbook.Sheets[sheet];
+
+      if (!worksheet) {
+        // If the worksheet doesn't exist, create a new one
+        worksheet = xlsx.utils.json_to_sheet([]);
+        xlsx.utils.book_append_sheet(workbook, worksheet, sheet);
+      }
+    } else {
+      workbook = xlsx.utils.book_new(); // Create a new workbook
+      worksheet = xlsx.utils.json_to_sheet([]); // Create a new sheet
+      xlsx.utils.book_append_sheet(workbook, worksheet, sheet); // Append the new sheet to the workbook
+    }
+
+    // Convert the new data to a format suitable for Excel
+    const newData = [data];
+    xlsx.utils.sheet_add_json(worksheet, newData, {
+      skipHeader: true,
+      origin: -1,
+    });
+
+    // Write the workbook to the file
+    xlsx.writeFile(workbook, backupFilePath);
 
     res.status(200).json({ message: "Data added successfully" });
   } catch (error) {
