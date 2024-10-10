3d56b8e5d26eefd57b9d42d4ec3362ef85d8a309
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-10-10 21:43:26 +0530

    frontend buttons added

--- Frontend/src/pages/CastSelectPage/CastSelectPage.jsx
+++ Frontend/src/pages/CastSelectPage/CastSelectPage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import  {useNavigate}  from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Image from "../../assets/Image.jpg";
 
 function CastSelectPage() {
- const navigate = useNavigate(); // Use 'const' to properly define the navigate function
+  const navigate = useNavigate(); // Use 'const' to properly define the navigate function
 
   return (
-    <div className="flex flex-col">
-      <div>
+    <div className="admin-dash-container">
+      <img src={Image} alt="Background" className="admin-dash-image" />
+      <div className="flex flex-col absolute">
+      <div >
         <button
+          className="m-5 bg-green-500 font-semibold text-white w-3/4 p-2 rounded-lg hover:bg-green-700"
           onClick={() => {
             navigate("/cast-one"); // Correct way to call navigate
           }}
@@ -17,6 +21,7 @@ function CastSelectPage() {
       </div>
       <div>
         <button
+          className="m-5 bg-green-500 font-semibold text-white p-2 w-3/4 rounded-lg hover:bg-green-700"
           onClick={() => {
             navigate("/cast-two"); // Correct way to call navigate
           }}
@@ -26,12 +31,14 @@ function CastSelectPage() {
       </div>
       <div>
         <button
+          className="m-5 bg-green-500 font-semibold text-white p-2 w-3/4 rounded-lg hover:bg-green-700"
           onClick={() => {
             navigate("/cast-three"); // Correct way to call navigate
           }}
         >
           Cast Billet Process Parameters
         </button>
+      </div>
       </div>
     </div>
   );
